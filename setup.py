1ef107714fd2e64c2e1ad61d11c57d2b1c2ac24d
Keisuke OGAKI <keisuke_ogaki@dwango.co.jp>
2015-10-17 23:22:02 +0900

    :star: setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='niconico_chainer_models',
+      version='0.0.1',
+      description='Model Definitions',
+      author='dwango',
+      author_email='',
+      url='',
+      packages=['niconico_chainer_models'],
+      install_requires=open('requirements.txt').read().splitlines(),
+      entry_points=""
+)
